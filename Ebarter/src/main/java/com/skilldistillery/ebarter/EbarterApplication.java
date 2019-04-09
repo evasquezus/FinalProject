968f2a74334f60029f4384f2976aafacc8f488fe
Edwin <giolambo@aol.com>
2019-04-09 16:27:04 -0600

    Stub JPA and Boot Project, link project dependency

---
+++ Ebarter/src/main/java/com/skilldistillery/ebarter/EbarterApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.ebarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbarterApplication.class, args);
+	}
+
+}
