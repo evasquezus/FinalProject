d8bf1a1cb8223b81e831560f8eb990d5d87cdb4d
John Overberg <john.richard.overberg@gmail.com>
2019-04-11 14:45:14 -0600

    initial angular push

---
+++ Angular/eBarter/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DatePipe, CurrencyPipe } from '@angular/common';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { LandingComponent } from './components/landing/landing.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    HomepageComponent,
+    ItemDetailComponent,
+    RegistrationComponent,
+    NavbarComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule.forRoot()
+  ],
+  providers: [,
+  DatePipe,
+  CurrencyPipe ],
+bootstrap: [AppComponent]
+})
+export class AppModule { }
