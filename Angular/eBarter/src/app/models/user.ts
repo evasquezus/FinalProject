acbf97e7b7c08f922766f937c62b596764085ce8
Yang Chen <yang.cl7@gmail.com>
2019-04-11 15:27:29 -0600

    finsh with angular models item, offer, user.ts

--- Angular/eBarter/src/app/models/user.ts
+++ Angular/eBarter/src/app/models/user.ts
@@ -1,2 +1,38 @@
 export class User {
+  id: number;
+  userName: string;
+  password: string;
+  active: boolean;
+  firstName: string;
+  lastName: string;
+  email: string;
+  registerDate: string;
+  roleId: number;
+  addressId: number;
+  authenticated: boolean;
+
+  constructor(
+    id?: number,
+    userName?: string,
+    password?: string,
+    active?: boolean,
+    firstName?: string,
+    lastName?: string,
+    email?: string,
+    registerDate?: string,
+    roleId?: number,
+    addressId?: number,
+    authenticated?: boolean) {
+      this.id = id;
+      this.userName = userName;
+      this.password = password;
+      this.active = active;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.registerDate = registerDate;
+      this.roleId = roleId;
+      this.addressId = addressId;
+      this.authenticated = authenticated;
+    }
 }
