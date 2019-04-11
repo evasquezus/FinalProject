55afa17ef1c1b72a6b34b99b501351301a4be3b9
John Overberg <john.richard.overberg@gmail.com>
2019-04-11 15:19:34 -0600

    routing works

--- Angular/eBarter/src/app/app-routing.module.ts
+++ Angular/eBarter/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { HomepageComponent } from './components/homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'item', component: ItemDetailComponent},
+  { path: 'register', component: RegistrationComponent },
+  { path: 'profile', component: ProfileComponent }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
