e31a3949b553927f2283b7e5069ae53d6e582b54
Edwin <giolambo@aol.com>
2019-04-12 20:58:34 -0600

    Set layout for stripe payment

---
+++ Ebarter/src/main/java/com/skilldistillery/ebarter/data/PaymentService.java
@@ -0,0 +1,8 @@
+//package com.skilldistillery.ebarter.data;
+//
+//public interface PaymentService {
+//
+//	public String createCustomer(User user);
+//
+//	public void chargeCreditCard(Order order);
+//}
