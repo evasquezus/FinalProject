e59c23c8d390e5d8f95fd9e37fb50d1f68de3ef8
John Overberg <john.richard.overberg@gmail.com>
2019-04-15 14:09:10 -0600

    profile admin features work

--- Angular/eBarter/src/app/components/profile/profile.component.ts
+++ Angular/eBarter/src/app/components/profile/profile.component.ts
@@ -23,14 +23,22 @@ export class ProfileComponent implements OnInit {
 
     user = new User();
     editUser: User;
+    isAdmin: boolean = false;
+    userList: User[];
+    selectedUser: User;
+    displayActive: string;
+    activeFormat: string;
 
 
   ngOnInit() {
     this.reload();
+    console.log('isAdmin: ' + this.isAdmin);
+
   }
 
   reload() {
     this.getCurrentUser();
+    this.getAllUsers();
     }
 
     getCurrentUser() {
@@ -39,6 +47,14 @@ export class ProfileComponent implements OnInit {
       response.subscribe(
         data => {
          this.user = data;
+         console.log('role id: ' + this.user.role.id);
+         if(this.user.role.id === 1) {
+           this.isAdmin = true;
+           this.getAllUsers();
+         }
+         else {
+           this.isAdmin = false;
+         }
          console.log(data);
 
         },
@@ -47,12 +63,70 @@ export class ProfileComponent implements OnInit {
       );
     }
 
+    getAllUsers() {
+      let response = this.userService.getAll();
+      response.subscribe(
+        data => {
+         this.userList = data;
+         console.log(this.userList);
+         this.selectedUser = this.userList[0];
+         if(this.selectedUser.enabled === true) {
+          this.displayActive = 'Active';
+          this.activeFormat = 'green';
+        }
+        else {
+          this.displayActive = 'Inactive';
+          this.activeFormat = 'red';
+        }
+        },
+        error => {
+        }
+      );
+    }
+
+    getSelectedUser(event: any) {
+      const userName = event.target.value;
+      console.log('get selected: ' + userName);
+      let response = this.userService.getUserByUserName(userName);
+      response.subscribe(
+        data => {
+        this.selectedUser = data;
+        this.getSelectedUserNoEvent();
+        },
+        error => {
+        }
+      );
+    }
+
+    getSelectedUserNoEvent() {
+      const userName = this.selectedUser.username;
+      console.log('get selected: ' + userName);
+      let response = this.userService.getUserByUserName(userName);
+      response.subscribe(
+        data => {
+        this.selectedUser = data;
+        if(this.selectedUser.enabled === true) {
+          this.displayActive = 'Active';
+          this.activeFormat = 'green';
+        }
+        else {
+          this.displayActive = 'Inactive';
+          this.activeFormat = 'red';
+        }
+
+        },
+        error => {
+        }
+      );
+    }
+
+
     navHome() {
       this.router.navigate(['home'])
     }
 
     updateUser() {
-      const userName = this.auth.getCredName();
+      // const userName = this.auth.getCredName();
       let response = this.userService.update(this.user);
       console.log('profile.component.updateUser() sending data');
 
@@ -67,6 +141,37 @@ export class ProfileComponent implements OnInit {
 
         }
       );
+    }
+
+    updateSelectedUser() {
+      console.log('in update selected');
+      // const userName = this.selectedUser.username;
+      let response = this.userService.update(this.selectedUser);
+      console.log('profile.component.updateSelectedUser() sending data');
+
+      response.subscribe(
+        data => {
+         this.selectedUser = data;
+         console.log(data);
+        this.getSelectedUserNoEvent();
+        },
+        error => {
+          console.log('error in profile.component.updateSelectedUser()');
+
+        }
+      );
+    }
+
+    activateUser() {
+      this.selectedUser.enabled = true;
+      console.log('activate: ' + this.selectedUser);
+      this.updateSelectedUser();
+    }
+
+    deactivateUser() {
+      this.selectedUser.enabled = false;
+      console.log('deactivate: ' + this.selectedUser);
+      this.updateSelectedUser();
     }
 
   // reload() {
